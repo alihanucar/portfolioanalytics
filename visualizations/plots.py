5e60ccdcf84333864c5318c4ab4845ae58ac7ae6
Alihan Ucar <alihanucaar@gmail.com>
2023-03-05 03:57:19 +0100

    Update website with my changes

--- visualizations/plots.py
+++ visualizations/plots.py
@@ -58,15 +58,15 @@ def basic_portfolio(stock_df):
     # Graphs the cumulative returns
     st.line_chart(cumulative_return)
 
-def portfolio_pie(choices):
-    """Uses the symbol weights to graph the portfolio composition as a pie chart.
-    """
-    symbols, weights = choices.values()
-    df = pd.DataFrame({'Symbol': symbols, 'Weight': weights})
-    fig, ax = plt.subplots(figsize=(8, 8))
-    ax = sns.pieplot(data=df, x='Weight', labels=df['Symbol'], autopct='%1.1f%%')
-    ax.set_title('Portfolio Composition')
-    st.pyplot(fig)
+#def portfolio_pie(choices):
+    #"""Uses the symbol weights to graph the portfolio composition as a pie chart.
+    #"""
+    #symbols, weights = choices.values()
+    #df = pd.DataFrame({'Symbol': symbols, 'Weight': weights})
+    #fig, ax = plt.subplots(figsize=(8, 8))
+    #ax = sns.pieplot(data=df, x='Weight', labels=df['Symbol'], autopct='%1.1f%%')
+    #ax.set_title('Portfolio Composition')
+    #st.pyplot(fig)
 
 
 
