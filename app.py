d1395c3bd2791a620d8f75b8916d0990ae8f8839
Alihan Ucar <alihanucaar@gmail.com>
2023-03-05 20:58:43 +0100

    Update website with my changes

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ def run():
     load_heading()
     choices = get_choices()
     if choices:
-        #portfolio_pie(choices['choices'])     
+        portfolio_pie(symbol_pie, weights_pie)     
         beta(choices['combined_df'])
         basic_portfolio(choices['combined_df'])
         display_heat_map(choices['combined_df'])
